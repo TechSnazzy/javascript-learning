7034b8934fe3c1004f4964af3c5d57f1027dc6e4
Sean Morrison <sean1972@gmail.com>
2021-08-23 22:22:18 -0700

    Create the basic HTML layout and add some horrible styling

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-var array = ['January', 'February', 'March', 'April', 'June', 'July', 'August'];
-
-function clickButton() {
-  for (var i = 0; i < array.length; i++) {
-    var el = document.getElementById('paragraph');
-    el.innerHTML = `It is ${array[i]}`;
-    console.log(array[i]);
-  }
-}
+// Create variables for the welcome message
