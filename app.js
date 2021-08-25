961de235703febd1fd28d6c4b7fd6525691dd519
Sean Morrison <sean1972@gmail.com>
2021-08-24 22:14:44 -0700

    Added members to the house object

--- app.js
+++ app.js
@@ -7,16 +7,26 @@ var house = {
     var roomsLeft = totalRooms - occupiedRooms;
     return roomsLeft;
   },
-  isOpen: false,
+  isOpen: true,
   name: 'Morrison Manor',
+  members: ['Sean', 'Stephanie', 'Emmie'],
 };
 
 var roomsAvailable = house.roomsAvailable();
 var occupiedRooms = house.occupiedRooms;
 var docMessage = document.getElementById('message');
+var person1 = house.members[0];
+var person2 = house.members[1];
+var person3 = house.members[2];
 
 if (house.isOpen === true) {
-  docMessage.textContent = `There are ${occupiedRooms} rooms occupied of ${house.totalRooms} total rooms leaving ${roomsAvailable} rooms available.`;
+  docMessage.textContent = `There are ${occupiedRooms} rooms occupied out of ${house.totalRooms} total rooms leaving ${roomsAvailable} rooms available for ${person1}, ${person2} and ${person3}.`;
 } else {
   docMessage.textContent = `${house.name} is not open.`;
 }
+
+function hello(a) {
+  return a;
+}
+
+console.log(hello('Test'));
