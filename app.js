69e9baa3a4b71f0c2413e98a0bcc19a539005d19
Sean Morrison <sean1972@gmail.com>
2021-08-27 16:53:57 -0700

    Added more items from the window

--- app.js
+++ app.js
@@ -4,8 +4,12 @@
 var msg = '<h2>Browser Window</h2>';
 
 // Add to the html code with more code.
-msg += '<p>This is a paragraph.</p>';
-msg += '<p>This is another paragraph.</p>'
+msg += '<p>The window height is:</p>' + window.innerHeight;
+msg += '<p>The window width is:</p>' + window.innerWidth;
+msg += '<p>The window screen width is:</p>' + window.screen.width;
+msg += '<p>The window screen height is:</p>' + window.screen.height;
+msg += '<p>The window history length is:</p>' + window.history.length;
+msg += '<p>The window location is:</p>' + window.location;
 
 // Select the #main ID.
 var el = document.getElementById('main');
