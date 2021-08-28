5a100e71347e7b5129aba63d992f885c89ef8c98
Sean Morrison <sean1972@gmail.com>
2021-08-28 13:08:57 -0700

    Create immediately invoked function

---
+++ app.js
@@ -0,0 +1,5 @@
+/* The script is placed inside an immediately invoked function expression which helps protect the scope of variables */
+
+(function () {
+  // create immediately invoked function
+}());
