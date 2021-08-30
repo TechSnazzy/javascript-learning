cf92232d3b736e6d55183669dd09ff6004a41bab
Sean Morrison <sean1972@gmail.com>
2021-08-29 22:29:57 -0700

    Complete the example

--- app.js
+++ app.js
@@ -32,13 +32,50 @@
 
   function offerExpires(today) {
     // Declare variables within the function for local scope
+    var weekFromToday, day, date, month, year, dayNames, monthNames;
+
     // Add 7 days time (added in milliseconds)
+    weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+
     // Create arrays to hold the names of days / months
+    dayNames = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+    monthNames = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+
     // Collect the parts of the date to show on the page
+    day = dayNames[weekFromToday.getDay()];
+    date = weekFromToday.getDate();
+    year = weekFromToday.getFullYear();
+
     // Create the message
+    expiryMsg = 'Offer expires next ';
+    expiryMsg += day + ' <br />(' + date + ' ' + month + ' ' + year + ')';
+    return expiryMsg;
   }
 
   today = new Date(); // Put today's date in variable
   elEnds = document.getElementById('offerEnds');
   elEnds.innerHTML = offerExpires(today); // Add the expiry message
+
+  // Finish the immediately invoked function expression
 })();
