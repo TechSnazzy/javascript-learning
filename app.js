3ef346ba26e2e01fc54d9d86f6f51a37771b0ab8
Sean Morrison <sean1972@gmail.com>
2021-09-01 22:10:29 -0700

    Example of using getElementsByTagName with a for loop

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-var el = document.getElementById('apple');
-el.className = 'alt-items';
+// var apple = document.getElementById('apple');
+// apple.className = 'apple';
+
+var groceryList = document.getElementsByTagName('li');
+
+for (var i = 0; i < groceryList.length; i++) {
+  groceryList[i].className = 'apple';
+}
