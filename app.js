ff4c9f6d8c37820125e7f86b07555c5791689205
Sean Morrison <sean1972@gmail.com>
2021-09-01 22:20:15 -0700

    Updated example with notes and array syntax vs item() method of using getElementsByTagName with a for loop

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // var apple = document.getElementById('apple');
 // apple.className = 'apple';
 
+// note: array syntax is faster than item() method
+
 var groceryList = document.getElementsByTagName('li');
 
 for (var i = 0; i < groceryList.length; i++) {
-  groceryList[i].className = 'apple';
+  groceryList[i].className = 'apple'; // array syntax
+  //groceryList.item(i).className = 'apple'; // item() method
 }
