96390af8ace134dac5961db8c3e182afcc14239e
Sean Morrison <sean1972@gmail.com>
2021-09-02 22:25:37 -0700

    Click a button to append an Apple list item to the list

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-// var apple = document.getElementById('apple');
-// apple.className = 'apple';
-
-// note: array syntax is faster than item() method
+/*
+Click an HTML button to append
+an Apple list item to the list.
+*/
 
 var fruitList = document.querySelectorAll('li.items');
 
-for (var i = 0; i < fruitList.length; i++) {
+for (var i = 0; i < fruitList.length - 1; i++) {
   fruitList[i].className = 'apple';
+}
+
+var firstItem = document.getElementById('apple');
+firstItem.textContent = '🫐 Blueberry';
+
+function addApple() {
+  // Create an li node
+  var apple = document.createElement('li');
+
+  // Create a text node
+  var textnode = document.createTextNode('🍎 Apple');
+
+  // Append the text node to the li node
+  apple.appendChild(textnode);
+
+  // Add a class to the lie
+  apple.className = 'apple';
+
+  // Select the myList ul
+  document.getElementById('myList');
+
+  // Append the apple list item to the list
+  myList.appendChild(apple);
 }
