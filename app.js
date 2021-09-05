04a68b5f4f708bd13d290b5b323dc1ee88306268
Sean Morrison <sean1972@gmail.com>
2021-09-05 14:58:46 -0700

    More examples of selecting specific items from the html

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// ===== EXAMPLE 1 ================================
 // Select the paragraph ID
 var para = document.getElementById('paragraph');
 
@@ -6,4 +7,14 @@ var para = document.getElementById('paragraph');
 var paraText = para.firstChild.nodeValue;
 
 // Display the text in the console
-console.log(paraText)
+console.log(paraText);
+
+// ===== EXAMPLE 2 ================================
+// Select all the list items
+var listItemOneText = document.getElementsByClassName('list-items');
+
+// Get the content of the first list item and store it in a variable
+var itemOne = listItemOneText[0].textContent;
+
+// Display in console
+console.log(itemOne);
