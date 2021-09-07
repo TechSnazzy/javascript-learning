45158467772a4fd1b3acc0c88333911a1892cb23
Sean Morrison <sean1972@gmail.com>
2021-09-06 21:57:57 -0700

    Add and remove newly created elements from an unordred list

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-// Create the apple element
+// STEP 1: Store the full list in the variable
+var getList = document.getElementById('list');
+
+// STEP 2: Then remove the first two list items
+var listItems = document.getElementsByTagName('li');
+getList.removeChild(listItems[0]);
+getList.removeChild(listItems[1]);
+
+// STEP 3: Create the apple element
 // Then create the apple text
 // Then add the apple text to the apple element
 var appleItem = document.createElement('li');
 var appleText = document.createTextNode('🍎 Apples');
 appleItem.appendChild(appleText);
 
-// Then get the list
-// And all the apple item to the list
-var getList = document.getElementById('list');
+// STEP 4: Add the apple item to the list
 getList.appendChild(appleItem);
-
-// Note: Remove an element with removeChild()
