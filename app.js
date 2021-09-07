d2235ad553272e7d058bd6919995e4ac89f21cd3
Sean Morrison <sean1972@gmail.com>
2021-09-06 21:41:07 -0700

    Create a new element and add it to an unordered list

---
+++ app.js
@@ -0,0 +1,11 @@
+// Create the apple element
+// Then create the apple text
+// Then add the apple text to the apple element
+var appleItem = document.createElement('li');
+var appleText = document.createTextNode('🍎 Apples');
+appleItem.appendChild(appleText);
+
+// Then get the list
+// And all the apple item to the list
+var getList = document.getElementById('list');
+getList.appendChild(appleItem);
