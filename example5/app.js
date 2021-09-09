51792ab727f67f521e03baa8988a44c0da534289
Sean Morrison <sean1972@gmail.com>
2021-09-09 15:28:19 -0700

    Add a new list item to the beginning of the list

--- example5/app.js
+++ example5/app.js
@@ -3,9 +3,18 @@ var list = document.getElementsByTagName('ul')[0];
 
 // Create a list item
 // Create a text node
-// Add the text node to the list item
-// Add the list item to the unordered list
+// Append the text node to the list item
+// Append the list item to END of the unordered list
 var newItemLast = document.createElement('li');
 var newTextLast = document.createTextNode('cream');
 newItemLast.appendChild(newTextLast);
 list.appendChild(newItemLast);
+
+// Create a list item
+// Create a text node
+// Append the text node to the list item
+// Insert the list item before the first child
+var newItemFirst = document.createElement('li');
+var newTextFirst = document.createTextNode('kale');
+newItemFirst.appendChild(newTextFirst);
+list.insertBefore(newItemFirst, list.firstChild);
