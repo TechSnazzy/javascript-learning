022d023f7330350bea02dd5854999caac48fe1bb
Sean Morrison <sean1972@gmail.com>
2021-09-09 22:09:51 -0700

    Refer to this example 5 for working with items in the DOM

--- example5/app.js
+++ example5/app.js
@@ -18,3 +18,23 @@ var newItemFirst = document.createElement('li');
 var newTextFirst = document.createTextNode('kale');
 newItemFirst.appendChild(newTextFirst);
 list.insertBefore(newItemFirst, list.firstChild);
+
+// Select all list items
+var listItems = document.querySelectorAll('li');
+
+// Add a class to all list items
+var i;
+for (i = 0; i < listItems.length; i++) {
+  listItems[i].className = 'cool';
+}
+
+// Select the h2 element
+// Then store the h2 text in a variable
+// Then store the list length in a variable
+// Then store the new heading string in a variable
+// Then add the heading html to the page
+var heading = document.querySelector('h2');
+var headingText = heading.firstChild.nodeValue;
+var totalItems = listItems.length;
+var newHeading = headingText + '<span>' + totalItems + '</span>';
+heading.innerHTML = newHeading;
