1f8cbf353bfb360527dabe27e7d76e99df92cb6b
Sean Morrison <sean1972@gmail.com>
2021-09-13 14:41:47 -0700

    Add some examples of capture true and stop propagation

--- event-listener-example/app.js
+++ event-listener-example/app.js
@@ -1,1 +1,36 @@
-console.log('hello');
+const grandparent = document.querySelector('.grandparent');
+const parent = document.querySelector('.parent');
+const child = document.querySelector('.child');
+
+grandparent.addEventListener('click', (e) => {
+  console.log('Grandparent bubble');
+});
+
+grandparent.addEventListener(
+  'click',
+  (e) => {
+    console.log('Grandparent capture');
+  },
+  { capture: true } // is false by default
+);
+
+parent.addEventListener('click', (e) => {
+  console.log('Parent bubble');
+});
+
+parent.addEventListener(
+  'click',
+  (e) => {
+    //e.stopPropagation(); // stop propagating
+    console.log('Parent capture');
+  },
+  { capture: true }
+);
+
+child.addEventListener('click', (e) => {
+  console.log('Child bubble');
+});
+
+document.addEventListener('click', (e) => {
+  console.log('Document bubble');
+});
