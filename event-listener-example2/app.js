b222e8e7c62a02569304c336f55d292708acac7e
Sean Morrison <sean1972@gmail.com>
2021-09-13 15:01:55 -0700

    More examples of capturing and bubbling

--- event-listener-example2/app.js
+++ event-listener-example2/app.js
@@ -1,2 +1,19 @@
+const grandparent = document.getElementById('grandparent');
 const parent = document.getElementById('parent');
 const child = document.getElementById('child');
+
+grandparent.addEventListener('click', function () {
+  console.log('I am the GRANDPARENT who is bubbling.');
+});
+
+parent.addEventListener(
+  'click',
+  function () {
+    console.log('I am the PARENT who is capturing.');
+  },
+  true
+);
+
+child.addEventListener('click', function () {
+  console.log('I am the CHILD who is bubbling.');
+});
