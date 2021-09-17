f76f72e3ca8899c9a80cf25e321ba38d78683ea9
Sean Morrison <sean1972@gmail.com>
2021-09-16 21:47:20 -0700

    Added a minimum length of characters to the function

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ var feedback = document.getElementById('feedback');
 Create a function that checks the
 length of the value of the username input
 */
-function checkUsername() {
+function checkUsername(minLength) {
   // explaining this.value.length
   // get the value of username
   // and get the length of the value
   // this refers to the element at the beginning of the event listener which is the object at the top of the global scope
-  if (this.value.length < 5) {
-    feedback.textContent = 'Username must be 5 characters or more!';
+  if (username.value.length < minLength) {
+    feedback.textContent =
+      'Username must be ' + minLength + ' characters or more!';
   } else {
     feedback.textContent = '';
   }
@@ -23,7 +24,13 @@ function checkUsername() {
 /*
 Handle the event with an event listener
 */
-username.addEventListener('blur', checkUsername, false);
+username.addEventListener(
+  'blur',
+  function () {
+    checkUsername(5);
+  },
+  false
+);
 
 /*
 Handle the event with an event handler
