7a5a5b7dc69d13329ba9bb1a987db51b14250bab
Sean Morrison <sean1972@gmail.com>
2021-09-22 21:14:36 -0700

    Adding some console log items to view various event objects

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+/*
+Select the button.
+*/
 var myBtn = document.getElementById('myBtn');
 
+/*
+Check the event object.
+*/
 myBtn.addEventListener('click', function (e) {
   console.log(e);
+  console.log('Bubbles are set to ' + e.bubbles);
+  if (e.ctrlKey) {
+    console.log('CTRL key was pressed while clicking.');
+  } else {
+    console.log('Nope, the CTRL key was not pressed.');
+  }
 });
