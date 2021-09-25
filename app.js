68e80ce0cd35b4502749c6d7ed3125d67cc5a618
Sean Morrison <sean1972@gmail.com>
2021-09-25 14:29:10 -0700

    Change background colors on click to help see what is happening visually

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const myBtn = document.getElementById('myBtn');
 // Add event listeners
 main.addEventListener('click', function () {
   console.log('Main was clicked!');
+  main.style.backgroundColor = 'cornsilk';
+  main.style.color = 'black';
 });
 
 myBtn.addEventListener('click', function (e) {
   e.stopPropagation();
   console.log('My button was clicked!');
+  main.style.backgroundColor = 'orange';
 });
