09a53054a156752a5dfccdedb87101786a08b2d4
Sean Morrison <sean1972@gmail.com>
2021-09-25 14:26:49 -0700

    Create event listeners

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 // Select elements
 const main = document.getElementById('main');
 const myBtn = document.getElementById('myBtn');
+
+// Add event listeners
+main.addEventListener('click', function () {
+  console.log('Main was clicked!');
+});
+
+myBtn.addEventListener('click', function () {
+  console.log('My button was clicked!');
+});
